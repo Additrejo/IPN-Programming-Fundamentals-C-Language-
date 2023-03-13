fa82ee8c8a6e8c36e40f020301f32dfa4ce17b62
Addi Trejo <125314660+Additrejo@users.noreply.github.com>
2023-03-13 00:43:34 -0600

    Add files via upload

---
+++ 1. hola mundo.cpp
@@ -0,0 +1,16 @@
+/*
+Instituto Politécnico Nacional. ESIME Z.
+Autor: Addi Trejo.
+Materia: Fundamentos de programación
+Created on Thu Feb 4 17:34:09 2016
+*/
+
+#include <stdio.h>                   //Esta biblioteca contiene funciones para la lectura y escritura de caracteres, cadenas, números y otros tipos de datos desde y hacia archivos y dispositivos de entrada/salida.
+#include <stdlib.h>                 //Esta biblioteca contiene funciones para la lectura y escritura de caracteres, cadenas, números y otros tipos de datos desde y hacia archivos y dispositivos de entrada/salida. En este progama sirve para system pause
+int main ()
+{
+ printf("\t Hola Mundo \n");       //"Printf" se utiliza para texto. \t se utiliza para dar un epacio, \n. se utiliza para un salto de linea.
+ system ("pause");                 //Se utiliza para agregar una pausa en la ejecución de un programa de consola de manera que el usuario tenga tiempo de revisar la salida
+ return 0;                         //Se utiliza para indicar que la ejecución del programa ha finalizado sin errores, y este valor se devuelve al sistema operativo para su posterior procesamiento.
+}
+ 
