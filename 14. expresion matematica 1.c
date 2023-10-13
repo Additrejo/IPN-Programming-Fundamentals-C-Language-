1f2ea325b1a6e3455ae77ce1dfe29c1d9b2b7c28
Addi Trejo <125314660+Additrejo@users.noreply.github.com>
2023-10-12 23:08:56 -0600

    Add files via upload

---
+++ 14. expresion matematica 1.c
@@ -0,0 +1,31 @@
+/* Instituto Politecnico Nacional. ESIME Z.
+Materia: Fundamentos de programacion.
+Autor: Addi Trejo.
+Programa: 14. Expresión matematica 1.
+Creado Feb 22 14:09:32 2016
+Descripcion: 1. Haga un programa que resuelva la siguiente expresión matematica.
+
+               a(r^n-1)
+      suma = -------------
+                   r-1
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float a,r,n,suma=0;
+	
+	printf("Expresion Matematica: suma = a(r^n-1)/ r-1 \n\n");
+	
+	printf("Ingrese el valor de a: "); scanf("%f",&a);
+	printf("Ingrese el valor de r: "); scanf("%f",&r);
+	printf("Ingrese el valor del exponente 'n': "); scanf("%f",&n);
+	
+	suma = a*((pow(r,n-1))/(r-1));
+	
+	printf("El resultado de la expresion matematica es: %.2f",suma);
+	
+	return 0;
+}
