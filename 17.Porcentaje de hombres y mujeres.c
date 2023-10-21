2afc43c6b25074415ce151e25c8770c2db0d4227
Addi Trejo <125314660+Additrejo@users.noreply.github.com>
2023-10-20 23:57:32 -0600

    Add files via upload

---
+++ 17.Porcentaje de hombres y mujeres.c
@@ -0,0 +1,37 @@
+/*
+-------------------------------------------------------------------------------------------------------------------
+Instituto Politecnico Nacional. ESIME Z.
+Materia: Fundamentos de programacion.
+Autor: Addi Trejo.
+Programa: 15.Calcular el porcentaje de hombres y mujeres.
+Creado Feb 20 23:39:02 2016
+Descripcion: Elabore un programa en C que calcule el porcentaje de la presencia de hombres y mujeres.
+----------------------------------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() 
+{
+   float PH, PM;
+   int NH,NM;
+   
+   printf("Programa para calcular el porcentaje de hombres y mujeres \n");
+   printf("\nIngrese el numero de Hombres:");
+   scanf("%d",&NH);
+   printf("Ingrese el numero de Mujeres:");
+   scanf("%d",&NM);
+   
+   PH=(NH*100)/(NH+NM);
+   PM=100-PH;
+   
+   printf("\n\nEl porcentaje de Hombres es: %.2f %%",PH);
+   printf("\nEl porcentaje de Mujeres es: %.2f %%",PM);
+   
+   
+    getch();	
+   	return 0;
+}
